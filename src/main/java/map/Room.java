f2d66d6f34592172e3b378d3a8721c0fa6d7688f
John Taggart <taggartj1985@gmail.com>
2020-06-04 16:01:47 +0100

    room added

--- src/main/java/map/Room.java
+++ src/main/java/map/Room.java
@@ -1,11 +1,31 @@
 package map;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Room {
-    ArrayList<Exit> exits;
+    private ArrayList<Exit> exits;
 
     public Room(){
         this.exits = new ArrayList<Exit>();
+    }
+
+    public ArrayList<Exit> getExits() {
+        return exits;
+    }
+
+    public int numberofExits(){
+        return this.exits.size();
+    }
+
+    public void findExit(){
+        this.exits = new ArrayList<Exit>();
+        for(Exits : randomNumber()){
+            this.exits.add(new Exit(randomNumber));
+        }
+    }
+
+    public void shuffleExits(){
+        Collections.shuffle(this.exits);
     }
 }
